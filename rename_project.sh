887d2627c330e8e272540208b250867e6f23780a
José Enrique Narváez Gago <jenarvaezg@gmail.com>
2019-02-16 20:12:51 +0100

    Add script to rename project

---
+++ rename_project.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+	echo "Usage: ./rename_project.sh <new_project_name>"
+	exit 1
+fi
+
+grep --include=*.py -lr scaffold . |  xargs sed -i '' -e "s/scaffold/$1/g"
+mv scaffold $1
